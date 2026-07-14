868c274ba147a0d4df4173ab2bb5c75bb8973a19
Jeferson Alves da Silva <jefersonalves344@gmail.com>
2026-07-14 07:08:28 -0300

    Add bar chart visualization for monthly expenses

---
+++ python-projects/expense-manager-python/graphics.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+
+
+categories = [
+"Food",
+"Internet",
+"Transport"
+]
+
+
+values = [
+100,
+80,
+40
+]
+
+
+plt.bar(
+categories,
+values
+)
+
+
+plt.title(
+"Monthly Expenses"
+)
+
+
+plt.show()
